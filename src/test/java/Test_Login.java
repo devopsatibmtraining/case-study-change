5c047905f66577a8cbf75f60d81cb4a72cc56924
devopsatibmtraining <48584667+devopsatibmtraining@users.noreply.github.com>
2019-03-28 10:23:01 +0800

    Update Test_Login.java

--- src/test/java/Test_Login.java
+++ src/test/java/Test_Login.java
@@ -19,4 +19,29 @@ public class Test_Login {
         guru_login check2 = new guru_login();
         assertNotEquals("Login is invalid", true, check2.isUserValid("wrong","mistake"));
     }
+    
+    @Test
+    public void test3() {
+
+        guru_login obj = new guru_login();
+        assertEquals("SPRINT", obj.getMessage("SPRINT"));
+
+    }
+
+    @Test
+    public void test4() {
+
+        guru_login obj = new guru_login();
+        assertEquals("MEDYO", obj.getMessage("MEDYO"));
+
+    }
+
+    @Test
+    public void test5() {
+
+        guru_login obj = new guru_login();
+        assertEquals("Please provide a name!", obj.getMessage(null));
+
+    }
+    
 }
