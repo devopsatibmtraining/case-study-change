e6991b42e54224ca3b27a11f4a3a054ff3afdc6d
devopsatibmtraining <48584667+devopsatibmtraining@users.noreply.github.com>
2019-03-28 10:48:30 +0800

    Update Test_Login.java

--- src/test/java/Test_Login.java
+++ src/test/java/Test_Login.java
@@ -31,8 +31,8 @@ public class Test_Login {
     @Test
     public void test4() {
 
-        guru_login check3 = new guru_login();
-        assertEquals("Login is valid", true, check3.isPasswordValid("true","mistake"));
+        guru_login check4 = new guru_login();
+        assertEquals("Login is valid", true, check4.isPasswordValid("true","mistake"));
 
     }
 
